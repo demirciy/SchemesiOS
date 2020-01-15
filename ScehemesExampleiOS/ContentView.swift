772e0080c42e351f527a018bce69604b1a5d070a
Yusuf Demirci <demirciy94@gmail.com>
2020-01-15 21:39:00 +0300

    Initial Commit

---
+++ ScehemesExampleiOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScehemesExampleiOS
+//
+//  Created by Yusuf Demirci on 15.01.2020.
+//  Copyright © 2020 Demirciy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
