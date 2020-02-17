83529aac58f70ba80dcaed0e2ed46b3a1dd7613a
Yusuf Demirci <demirciy94@gmail.com>
2020-02-17 23:20:15 +0300

    - Added base URL support by scheme.

--- ScehemesExampleiOS/ContentView.swift
+++ ScehemesExampleiOS/ContentView.swift
@@ -9,8 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var baseURL: String {
+        #if DEBUG
+        return "This is DEBUG base URL"
+        #elseif TEST
+        return "This is TEST base URL"
+        #elseif RELEASE
+        return "This is RELEASE base URL"
+        #else
+        return "This is unknown base URL"
+        #endif
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        Text(baseURL)
     }
 }
 
